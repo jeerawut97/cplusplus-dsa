c220c6573b4b350d3ad3732c8582a37a583dd472
JEE <jeerawut.jd40@gmail.com>
2024-04-20 15:25:14 +0700

    variable type

--- 101-basic-programming/hello.cpp
+++ 101-basic-programming/hello.cpp
@@ -3,9 +3,17 @@
 using namespace std;
 
 int main() {
-    int age;
+    float age;
     cout << "How old are you?\n";
     cin >> age;
-    cout << "I'm year old " << age;
+    cout << "I'm year old " << age << "\n";
+
+    char favorite;
+    cout << "What favorite character?" << endl;
+    cin >> favorite;
+    cout << "I like " << favorite << endl;
+
+    bool is_can;
+    cout << "type bool can assigne 0 or 1" << endl;
     return 0;
 }
