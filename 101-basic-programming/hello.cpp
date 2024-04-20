27b36d106c201a15b0deb80e54402ad575cbb8d6
JEE <jeerawut.jd40@gmail.com>
2024-04-20 15:01:18 +0700

    handle input

--- 101-basic-programming/hello.cpp
+++ 101-basic-programming/hello.cpp
@@ -1,6 +1,11 @@
 #include<iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello EIEI!";
+    int age;
+    cout << "How old are you?\n";
+    cin >> age;
+    cout << "I'm year old " << age;
     return 0;
 }
